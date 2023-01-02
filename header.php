61bc2b2d882768a804f6e80fb1d3cc736f7dcd57
Ashish <ashishvishwakarma.cs@gmail.com>
2023-01-02 23:48:07 +0530

    Update header.php
    all headers

--- header.php
+++ header.php
@@ -2,3 +2,12 @@
 header('X-Frame-Options: DENY');
 #.Content Security Policy (CSP)
 header("Content-Security-Policy: frame-ancestors 'none'");
+
+
+#all require security headers
+header('X-Frame-Options: DENY');
+header("Content-Security-Policy: frame-ancestors 'none'");
+header("Strict-Transport-Security: max-age=63072000; includeSubDomains; preload");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: no-referrer-when-downgrade");
+header("Permissions-Policy: self");
